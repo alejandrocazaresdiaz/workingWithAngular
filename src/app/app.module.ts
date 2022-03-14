cab333253389782c6a1ee09d550eef98e6de7c41
Martin Alejandro Cazares Diaz <alejandro.cazares@gdl.dextratech.com>
2022-03-14 15:22:08 -0600

    Including routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    CursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
